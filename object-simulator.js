61019e33f0bb21931b9d0d7acaf2c098d481910d
leoying <934725772@qq.com>
2022-09-21 11:35:56 +0800

    增加几个新的方法

--- object-simulator.js
+++ object-simulator.js
@@ -2,8 +2,37 @@ const ObjectSimulator(function() {
     
   function create(o) {
      function F(){}
-    F.prototype = o;
+     F.prototype = o;
+     return new F();
   }
 
-
+  function getPrototypeOf(f) {
+    return f.constructor.prototye;
+  }
+    
+  /* a {}
+   * b function
+   */
+  function instanceOf(a, b) {
+     const proto = getPrototypeOf(a);
+     const prototype = b.prototype;
+     
+     while(true) {
+        if (!proto) {
+          return false;
+        }
+        if (proto === prototype) {
+           return true;
+        }
+         
+        proto = getPrototypeOf(proto)
+     }
+  }
+    
+    
+  return {
+    create,
+    getPrototypeOf,
+    instanceOf
+  }
 })()
